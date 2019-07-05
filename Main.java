7acade9dbbeda76eb53afd864e2e00c4082208c5
遠藤 拓実 <endotakumi@wlan-napt-003.u-aizu.ac.jp>
2019-07-05 16:11:32 +0900

    Won/Lost Message Added

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
-import java.util.Random;
+	import java.util.Random;
 
-public class Main {
+	public class Main {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-Random rand = new Random();
-int D1=rand.nextInt(6)+1;
-int D2=rand.nextInt(6)+1;
-int total=D1+D2;
-System.out.println("Rolling the dice...");
-System.out.println("Die 1: "+D1);
-System.out.println("Die 2: "+D2);
-System.out.println("Total value: "+total);
-	}
-
-}
+		public static void main(String[] args) {
+			// TODO 自動生成されたメソッド・スタブ
+	Random rand = new Random();
+	int D1=rand.nextInt(6)+1;
+	int D2=rand.nextInt(6)+1;
+	int total=D1+D2;
+	System.out.println("Rolling the dice...");
+	System.out.println("Die 1: "+D1);
+	System.out.println("Die 2: "+D2);
+	System.out.println("Total value: "+total);
+		if(total>7) {
+			System.out.println("You won");
+			} else {
+				System.out.println("You lost");
+			}
+		}
+		}
