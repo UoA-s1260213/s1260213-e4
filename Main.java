c919212a484e368b832154ad0376c7d216b04363
遠藤 拓実 <endotakumi@wlan-napt-003.u-aizu.ac.jp>
2019-07-05 15:54:12 +0900

    initial revision

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+Random rand = new Random();
+int D1=rand.nextInt(6)+1;
+int D2=rand.nextInt(6)+1;
+int total=D1+D2;
+System.out.println("Rolling the dice...");
+System.out.println("Die 1: "+D1);
+System.out.println("Die 2: "+D2);
+System.out.println("Total value: "+total);
+	}
+
+}
